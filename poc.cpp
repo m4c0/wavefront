3aee027f6e8f3c718f4489d3f2e52fe8bb9dc216
Eduardo Costa <m4c0@users.noreply.github.com>
2025-03-31 23:40:28 +0100

    buffer for 3d model

--- poc.cpp
+++ poc.cpp
@@ -2,19 +2,66 @@
 #pragma leco add_shader "poc.vert"
 #pragma leco add_shader "poc.frag"
 
+import dotz;
+import traits;
 import vee;
 import voo;
 import vapp;
 
+struct vtx {
+  dotz::vec3 pos;
+};
+
+static unsigned load_model(voo::h2l_buffer & buf) {
+  unsigned count {};
+  voo::memiter<vtx> m { buf.host_memory(), &count };
+  m += { .pos { -1, -1, 0 } };
+  m += { .pos { +1, +1, 0 } };
+  m += { .pos { +1, -1, 0 } };
+
+  m += { .pos { -1, -1, 0 } };
+  m += { .pos { +1, +1, 0 } };
+  m += { .pos { -1, +1, 0 } };
+  return count;
+}
+
 struct lng : public vapp {
   void run() override {
     main_loop("poc-voo", [&](auto & dq, auto & sw) {
+      voo::h2l_buffer buf { dq.physical_device(), sizeof(vtx) * 1024 };
+      int count = load_model(buf);
+
       auto pl = vee::create_pipeline_layout();
-      voo::one_quad_render oqr { "poc", &dq, *pl };
+      auto gp = vee::create_graphics_pipeline({
+        .pipeline_layout = *pl,
+        .render_pass = dq.render_pass(),
+        .back_face_cull = false,
+        .shaders {
+          voo::shader("poc.vert.spv").pipeline_vert_stage(),
+          voo::shader("poc.frag.spv").pipeline_frag_stage(),
+        },
+        .bindings {
+          vee::vertex_input_bind(sizeof(vtx)),
+        },
+        .attributes {
+          vee::vertex_attribute_vec3(0, traits::offset_of(&vtx::pos)),
+        },
+      });
+
+      bool loaded = false;
       extent_loop(dq.queue(), sw, [&] {
         sw.queue_one_time_submit(dq.queue(), [&](auto pcb) {
+          if (!loaded) {
+            buf.setup_copy(*pcb);
+            loaded = true;
+          }
+
           auto scb = sw.cmd_render_pass({ *pcb });
-          oqr.run(*pcb, sw.extent());
+          vee::cmd_set_viewport(*pcb, sw.extent());
+          vee::cmd_set_scissor(*pcb, sw.extent());
+          vee::cmd_bind_vertex_buffers(*pcb, 0, buf.local_buffer());
+          vee::cmd_bind_gr_pipeline(*pcb, *gp);
+          vee::cmd_draw(*pcb, count);
         });
       });
     });
--- poc.vert
+++ poc.vert
@@ -2,7 +2,7 @@
 #extension GL_GOOGLE_include_directive : require
 #include "../glslinc/3d.glsl"
 
-layout(location = 0) in vec2 pos;
+layout(location = 0) in vec3 pos;
 
 const float fov_rad = radians(90);
 const float aspect = 1;
@@ -15,5 +15,5 @@ void main() {
   vec4 cam = vec4(0, 0, -5, 0);
   mat4 proj = projection_matrix(fov_rad, aspect, near, far);
   mat4 view = view_matrix(cam.xyz, radians(cam.w), up);
-  gl_Position = vec4(pos.x, -pos.y, 0, 1) * view * proj;
+  gl_Position = vec4(pos.x, -pos.y, pos.z, 1) * view * proj;
 }
