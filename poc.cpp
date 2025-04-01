420b12d40f14adad625054bd6d5e4c1691fc1353
Eduardo Costa <m4c0@users.noreply.github.com>
2025-04-02 00:17:41 +0100

    pre-stiching model

--- poc.cpp
+++ poc.cpp
@@ -4,6 +4,7 @@
 #pragma leco add_resource "model.obj"
 
 import dotz;
+import hai;
 import jojo;
 import jute;
 import sires;
@@ -47,83 +48,67 @@ static constexpr float atof(jute::view v) {
   return negative ? -res : res;
 }
 
-static auto count_points() {
-  struct {
-    unsigned v_count {};
-    unsigned x_count {};
-  } res {};
+static vtx read_vertex(auto & pos, auto & txt, jute::view vtn) {
+  auto [v, vr] = vtn.split('/');
+  auto [t, tr] = vr.split('/');
+  // auto [n, nr] = tr.split('/');
+  
+  return vtx {
+    .pos = pos.seek(atoi(v) - 1),
+    .txt = txt.seek(atoi(t) - 1),
+  };
+}
+static auto read_model() {
+  hai::chain<dotz::vec3> pos { 10240 };
+  hai::chain<dotz::vec2> txt { 10240 };
+  hai::chain<vtx> vtx { 102400 };
 
   jojo::readlines(sires::real_path_name("model.obj"), [&](auto line) {
     if (line.size() < 2) return;
-    if (line[0] == 'v' && line[1] == ' ') res.v_count++;
-    if (line[0] == 'f' && line[1] == ' ') {
-      res.x_count += 3;
-      if (line.subview(1).after
-          .split(' ').after
-          .split(' ').after
-          .split(' ').after
-          .size()) res.x_count += 3;
-    }
-  });
-
-  return res;
-}
-
-static void load_model(voo::h2l_buffer & buf) {
-  voo::mapmem mm { buf.host_memory() };
-  auto * mv = static_cast<vtx *>(*mm);
-  auto * mt = static_cast<vtx *>(*mm);
-
-  jojo::readlines(sires::real_path_name("model.obj"), [&](auto line) {
     auto [cmd, vr] = line.split(' ');
     auto [x, xr] = vr.split(' ');
     auto [y, yr] = xr.split(' ');
     if (cmd == "v") {
       auto [z, zr] = yr.split(' ');
       dotz::vec3 p { atof(x), atof(y), atof(z) };
-      (*mv++).pos = p * 100.0;
+      pos.push_back(p * 100.0);
     } else if (cmd == "vt") {
-      (*mt++).txt = { atof(x), atof(y) };
+      txt.push_back({ atof(x), atof(y) });
+    } else if (cmd == "f") {
+      auto [z, zr] = yr.split(' ');
+      auto [w, wr] = zr.split(' ');
+      auto v0 = read_vertex(pos, txt, x);
+      auto v1 = read_vertex(pos, txt, y);
+      auto v2 = read_vertex(pos, txt, z);
+      vtx.push_back(v0);
+      vtx.push_back(v1);
+      vtx.push_back(v2);
+      if (!w.size()) return;
+      auto v3 = read_vertex(pos, txt, w);
+      vtx.push_back(v0);
+      vtx.push_back(v3);
+      vtx.push_back(v2);
     }
   });
+
+  return vtx;
 }
 
-static void load_indices(voo::h2l_buffer & buf) {
-  unsigned count {};
-  voo::memiter<unsigned> m { buf.host_memory(), &count };
-
-  jojo::readlines(sires::real_path_name("model.obj"), [&](auto line) {
-    if (line[0] != 'f' || line[1] != ' ') return;
-    auto [f, fr] = line.split(' ');
-    auto [v0, v0r] = fr.split(' ');
-    auto [v1, v1r] = v0r.split(' ');
-    auto [v2, v2r] = v1r.split(' ');
-    auto [v3, v3r] = v2r.split(' ');
-
-    auto i0 = atoi(v0.split('/').before);
-    auto i1 = atoi(v1.split('/').before);
-    auto i2 = atoi(v2.split('/').before);
-    auto i3 = atoi(v3.split('/').before);
-    m += i0 - 1U; m += i1 - 1U; m += i2 - 1U;
-    if (i3) m += i3 - 1U; m += i0 - 1U; m += i2 - 1U;
-  });
+static void load_model(voo::h2l_buffer & buf, auto & vs) {
+  voo::mapmem mm { buf.host_memory() };
+  auto * m = static_cast<vtx *>(*mm);
+  for (auto v : vs) *m++ = v;
 }
 
 struct lng : public vapp {
   void run() override {
     main_loop("poc-voo", [&](auto & dq, auto & sw) {
-      auto [v_count, x_count] = count_points();
+      auto vs = read_model();
 
+      unsigned v_count = vs.size();
       unsigned v_size = v_count * sizeof(vtx);
       voo::h2l_buffer v_buf { dq.physical_device(), v_size };
-      load_model(v_buf);
-
-      unsigned x_size = x_count * sizeof(unsigned);
-      voo::h2l_buffer x_buf {
-        dq.physical_device(), x_size,
-        vee::buffer_usage::index_buffer
-      };
-      load_indices(x_buf);
+      load_model(v_buf, vs);
 
       auto pl = vee::create_pipeline_layout({
         vee::vertex_push_constant_range<float>()
@@ -151,7 +136,6 @@ struct lng : public vapp {
         sw.queue_one_time_submit(dq.queue(), [&](auto pcb) {
           if (!loaded) {
             v_buf.setup_copy(*pcb);
-            x_buf.setup_copy(*pcb);
             loaded = true;
           }
           float ms = t.millis() / 1000.0;
@@ -160,10 +144,9 @@ struct lng : public vapp {
           vee::cmd_set_viewport(*pcb, sw.extent());
           vee::cmd_set_scissor(*pcb, sw.extent());
           vee::cmd_bind_vertex_buffers(*pcb, 0, v_buf.local_buffer());
-          vee::cmd_bind_index_buffer_u32(*pcb, x_buf.local_buffer());
           vee::cmd_bind_gr_pipeline(*pcb, *gp);
           vee::cmd_push_vertex_constants(*pcb, *pl, &ms);
-          vee::cmd_draw_indexed(*pcb, x_count);
+          vee::cmd_draw(*pcb, v_count);
         });
       });
     });
