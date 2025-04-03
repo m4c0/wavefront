b3dcd264f00bf00e3d996e917c7b3be0b9531375
Eduardo Costa <m4c0@users.noreply.github.com>
2025-04-03 22:57:28 +0100

    tweaks to resize

--- resize.cpp
+++ resize.cpp
@@ -7,21 +7,21 @@ import jute;
 
 using namespace jute::literals;
 
+static constexpr jute::view out = "../boosh/bullet.obj";
+
 static void write_line(auto line) {
-  jojo::append("out/model.obj", line);
-  jojo::append("out/model.obj", "\n"_hs);
+  jojo::append(out, line);
+  jojo::append(out, "\n"_hs);
 }
 
-static void write_n(jute::view f, char c) {
-  float n = jute::to_f(f) * 10;
-
+static void write_n(float n, char c) {
   char buf[256];
   snprintf(buf, 256, "%0.6f%c", n, c);
-  jojo::append("out/model.obj", jute::view { buf, strlen(buf) });
+  jojo::append(out, jute::view { buf, strlen(buf) });
 }
 
 int main(int argc, char ** argv) {
-  jojo::write("out/model.obj", ""_hs);
+  jojo::write(out, ""_hs);
 
   jojo::readlines("model.obj", [&](auto line) {
     if (line.size() < 2) return write_line(line);
@@ -33,9 +33,9 @@ int main(int argc, char ** argv) {
     auto [y, yr] = xr.split(' ');
     auto [z, zr] = yr.split(' ');
 
-    jojo::append("out/model.obj", "v "_hs);
-    write_n(x, ' ');
-    write_n(y, ' ');
-    write_n(z, '\n');
+    jojo::append(out, "v "_hs);
+    write_n(jute::to_f(x) * 10 + 0.05, ' ');
+    write_n(jute::to_f(y) * 10 - 1.00, ' ');
+    write_n(jute::to_f(z) * 10 + 0.05, '\n');
   });
 }
